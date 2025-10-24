3b2e237dd1c97dcd3fd2409a3b17abd718370d14
Luis Martins <luismwolf@gmail.com>
2025-10-23 23:38:30 -0400

    tune up theme

---
+++ lua/config/highlights.lua
@@ -0,0 +1,142 @@
+-- lua/config/highlights.lua
+-- Horizon-leaning highlights (red/orange bias), no changes to your tokyonight config
+
+local P = {
+  -- UI / base
+  bg            = "#1C1E26",
+  bg_alt        = "#232530",
+  border        = "#1A1C23",
+  accent        = "#2E303E",
+  accentAlt     = "#6C6F93",
+  lightText     = "#D5D8DA",
+  gray          = "#BBBBBB",
+
+  -- Horizon hues
+  cranberry     = "#E95678", -- reddish pink
+  apricot       = "#F09483",
+  rosebud       = "#FAB795",
+  tacao         = "#FAC29A",
+  lavender      = "#B877DB",
+  turquoise     = "#25B0BC",
+
+  -- extras from theme
+  cursorline_bg = "#21232D",
+  visual_bg     = "#343647",
+  match_paren   = "#44475D",
+  active_ln     = "#fa8d1a",
+  inactive_ln   = "#4d4242",
+  string_fg     = "#E4A88A",
+  type_fg       = "#E4B28E",
+  tag_fg        = "#D55070",
+  negative      = "#F43E5C",
+  warning_ok    = "#27D797",
+}
+
+local overrides = {
+  --------------------------------------------------------------------------
+  -- CORE UI / TEXT
+  --------------------------------------------------------------------------
+  Normal        = { fg = P.gray, bg = P.bg },
+  NormalNC      = { fg = P.gray, bg = P.bg },
+  NormalFloat   = { fg = P.gray, bg = P.bg_alt },
+  NormalSB      = { fg = P.active_ln, bg = P.bg_alt },
+
+  Comment       = { fg = "#4C4D53", italic = true },
+  CursorLine    = { bg = P.cursorline_bg },
+  CursorLineNr  = { fg = P.active_ln, bold = true },
+  LineNr        = { fg = P.inactive_ln },
+  LineNrAbove   = { fg = P.inactive_ln },
+  LineNrBelow   = { fg = P.inactive_ln },
+  Visual        = { bg = P.visual_bg },
+  VisualNOS     = { bg = P.visual_bg },
+  MatchParen    = { fg = P.rosebud, bg = P.match_paren, bold = true },
+
+  Search        = { fg = P.bg, bg = P.rosebud },
+  IncSearch     = { fg = P.bg, bg = P.cranberry },
+
+  -- push the syntax toward Horizon's reds/oranges/pinks
+  String        = { fg = P.string_fg },
+  Constant      = { fg = P.apricot },
+  Type          = { fg = P.type_fg },
+  Keyword       = { fg = P.lavender, italic = true },
+  Identifier    = { fg = P.tag_fg },     -- pink identifiers/tags
+  Function      = { fg = P.turquoise },  -- red-leaning funcs (less teal)
+  Operator      = { fg = P.gray },
+  Delimiter     = { fg = "#6C6D71" },
+  
+  ["@variable"]                = { fg = P.tag_fg },
+  ["@lsp.type.variable"]       = { fg = P.tag_fg },
+  ["@lsp.typemod.variable"]    = { fg = P.tag_fg },
+  ["@variable.parameter"]      = { fg = P.tag_fg },
+  ["@variable.member"]         = { fg = P.tag_fg },
+  ["@property"]         = { fg = P.tag_fg },
+
+
+
+  --------------------------------------------------------------------------
+  -- DIAGNOSTICS
+  --------------------------------------------------------------------------
+  DiagnosticError            = { fg = P.negative },
+  DiagnosticWarn             = { fg = P.rosebud },
+  DiagnosticInfo             = { fg = P.lavender },
+  DiagnosticHint             = { fg = P.turquoise },
+
+  DiagnosticUnderlineError   = { undercurl = true, sp = P.negative },
+  DiagnosticUnderlineWarn    = { undercurl = true, sp = P.rosebud },
+  DiagnosticUnderlineInfo    = { undercurl = true, sp = P.lavender },
+  DiagnosticUnderlineHint    = { undercurl = true, sp = P.turquoise },
+
+  DiagnosticVirtualTextError = { fg = P.negative, bg = "#4A2024" },
+  DiagnosticVirtualTextWarn  = { fg = P.rosebud,  bg = "#3A2D21" },
+  DiagnosticVirtualTextInfo  = { fg = P.lavender, bg = "#392636" },
+  DiagnosticVirtualTextHint  = { fg = P.turquoise, bg = "#23383B" },
+
+  --------------------------------------------------------------------------
+  -- POPUPS / PMENU
+  --------------------------------------------------------------------------
+  FloatBorder   = { fg = P.bg_alt, bg = P.bg_alt },
+  FloatTitle    = { fg = P.lavender, bg = P.bg_alt, bold = true },
+  Pmenu         = { fg = P.gray, bg = P.bg_alt },
+  PmenuSel      = { fg = P.cranberry, bg = P.accent },
+  PmenuSbar     = { bg = "#242631" },
+  PmenuThumb    = { bg = P.match_paren },
+
+  --------------------------------------------------------------------------
+  -- DASHBOARD / ALPHA
+  --------------------------------------------------------------------------
+  AlphaHeader   = { fg = P.cranberry },
+  AlphaButtons  = { fg = P.tag_fg },
+  AlphaFooter   = { fg = P.accentAlt, italic = true },
+  AlphaShortcut = { fg = P.rosebud },
+
+  --------------------------------------------------------------------------
+  -- TELESCOPE
+  --------------------------------------------------------------------------
+  TelescopeNormal       = { fg = P.gray, bg = P.bg_alt },
+  TelescopeBorder       = { fg = P.bg_alt, bg = P.bg_alt },
+  TelescopePromptBorder = { fg = P.accent, bg = P.bg_alt },
+  TelescopePromptTitle  = { fg = P.cranberry, bg = P.bg_alt, bold = true },
+  TelescopeResultsComment = { fg = "#4C4D53" },
+  TelescopeSelection    = { fg = P.lightText, bg = P.accent },
+  TelescopeMatching     = { fg = P.cranberry, bold = true },
+
+  --------------------------------------------------------------------------
+  -- STATUS/TABS
+  --------------------------------------------------------------------------
+  StatusLine    = { fg = P.active_ln, bg = P.bg },
+  StatusLineNC  = { fg = P.inactive_ln, bg = P.bg },
+  TabLine       = { fg = P.accentAlt, bg = P.border },
+  TabLineFill   = { bg = P.bg },
+  TabLineSel    = { fg = P.lightText, bg = P.bg, bold = true },
+
+  --------------------------------------------------------------------------
+  -- MISC
+  --------------------------------------------------------------------------
+  Directory     = { fg = P.cranberry },
+  Title         = { fg = P.tag_fg, bold = true },
+  WarningMsg    = { fg = P.warning_ok },
+  ErrorMsg      = { fg = P.negative, bold = true },
+  Question      = { fg = P.lavender },
+}
+
+return overrides
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -5,7 +5,7 @@ return {
   --   opts = require "configs.conform",
   -- },
 
-  { "neovim/nvim-lspconfig"},
+  { "neovim/nvim-lspconfig" },
   {
     "nvim-treesitter/nvim-treesitter",
     opts = {
@@ -62,9 +62,9 @@ return {
     lazy = false,
     config = function()
       -- Use one of the methods in the Integration section to compose the command.
-      local mason_registry = require "mason-registry"
+      local mason_registry = require("mason-registry")
 
-      local rzls_path = vim.fn.expand "$MASON/packages/rzls/libexec"
+      local rzls_path = vim.fn.expand("$MASON/packages/rzls/libexec")
       local cmd = {
         "roslyn",
         "--stdio",
@@ -78,7 +78,7 @@ return {
 
       vim.lsp.config("roslyn", {
         cmd = cmd,
-        handlers = require "rzls.roslyn_handlers",
+        handlers = require("rzls.roslyn_handlers"),
         settings = {
           ["csharp|inlay_hints"] = {
             csharp_enable_inlay_hints_for_implicit_object_creation = true,
@@ -100,23 +100,23 @@ return {
           },
         },
       })
-      vim.lsp.enable "roslyn"
+      vim.lsp.enable("roslyn")
     end,
     init = function()
       -- We add the Razor file types before the plugin loads.
-      vim.filetype.add {
+      vim.filetype.add({
         extension = {
           razor = "razor",
           cshtml = "razor",
         },
-      }
+      })
     end,
   },
   { "nvim-telescope/telescope-ui-select.nvim" },
   {
-  "folke/persistence.nvim",
-  event = "BufReadPre",
-  opts = {},
+    "folke/persistence.nvim",
+    event = "BufReadPre",
+    opts = {},
   -- stylua: ignore
   keys = {
     { "<leader>qs", function() require("persistence").load() end, desc = "Restore Session" },
@@ -124,37 +124,37 @@ return {
     { "<leader>ql", function() require("persistence").load({ last = true }) end, desc = "Restore Last Session" },
     { "<leader>qd", function() require("persistence").stop() end, desc = "Don't Save Current Session" },
   },
-},
-{
-  "folke/noice.nvim",
-  event = "VeryLazy",
-  opts = {
-    lsp = {
-      override = {
-        ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-        ["vim.lsp.util.stylize_markdown"] = true,
-        ["cmp.entry.get_documentation"] = true,
-      },
-    },
-    routes = {
-      {
-        filter = {
-          event = "msg_show",
-          any = {
-            { find = "%d+L, %d+B" },
-            { find = "; after #%d+" },
-            { find = "; before #%d+" },
-          },
-        },
-        view = "mini",
-      },
-    },
-    presets = {
-      bottom_search = true,
-      command_palette = true,
-      long_message_to_split = true,
-    },
   },
+  {
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    opts = {
+      lsp = {
+        override = {
+          ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+          ["vim.lsp.util.stylize_markdown"] = true,
+          ["cmp.entry.get_documentation"] = true,
+        },
+      },
+      routes = {
+        {
+          filter = {
+            event = "msg_show",
+            any = {
+              { find = "%d+L, %d+B" },
+              { find = "; after #%d+" },
+              { find = "; before #%d+" },
+            },
+          },
+          view = "mini",
+        },
+      },
+      presets = {
+        bottom_search = true,
+        command_palette = true,
+        long_message_to_split = true,
+      },
+    },
   -- stylua: ignore
   keys = {
     { "<leader>sn", "", desc = "+noice"},
@@ -167,30 +167,31 @@ return {
     { "<c-f>", function() if not require("noice.lsp").scroll(4) then return "<c-f>" end end, silent = true, expr = true, desc = "Scroll Forward", mode = {"i", "n", "s"} },
     { "<c-b>", function() if not require("noice.lsp").scroll(-4) then return "<c-b>" end end, silent = true, expr = true, desc = "Scroll Backward", mode = {"i", "n", "s"}},
   },
-  config = function(_, opts)
-    -- HACK: noice shows messages from before it was enabled,
-    -- but this is not ideal when Lazy is installing plugins,
-    -- so clear the messages in this case.
-    if vim.o.filetype == "lazy" then
-      vim.cmd([[messages clear]])
-    end
-    require("noice").setup(opts)
-    local orig = vim.lsp.handlers["$/progress"]
-vim.lsp.handlers["$/progress"] = function(err, result, ctx, config)
-  if not (result and result.token) then return end
-  return orig(err, result, ctx, config)
-end
-
-  end,
-},
-{
-  "goolord/alpha-nvim",
-  event = "VimEnter",
-  enabled = true,
-  init = false,
-  opts = function()
-    local dashboard = require("alpha.themes.dashboard")
-    local logo = [[
+    config = function(_, opts)
+      -- HACK: noice shows messages from before it was enabled,
+      -- but this is not ideal when Lazy is installing plugins,
+      -- so clear the messages in this case.
+      if vim.o.filetype == "lazy" then
+        vim.cmd([[messages clear]])
+      end
+      require("noice").setup(opts)
+      local orig = vim.lsp.handlers["$/progress"]
+      vim.lsp.handlers["$/progress"] = function(err, result, ctx, config)
+        if not (result and result.token) then
+          return
+        end
+        return orig(err, result, ctx, config)
+      end
+    end,
+  },
+  {
+    "goolord/alpha-nvim",
+    event = "VimEnter",
+    enabled = true,
+    init = false,
+    opts = function()
+      local dashboard = require("alpha.themes.dashboard")
+      local logo = [[
 
   ⠀⠲⣦⣤⣀⣀⠀⠀⠀⣀⣀⣠⣤⣀⣀⠀⢀⣀⣠⣤⣶⣶⠟⠀⠀⠀
 ⠀⠀⠀⠀⠙⣿⣿⣿⣿⣷⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠋⠀⠀⠀⠀
@@ -205,7 +206,7 @@ end
 ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠹⠀⠀⠀⠀⠀⠀⠀
     ]]
 
-    dashboard.section.header.val = vim.split(logo, "\n")
+      dashboard.section.header.val = vim.split(logo, "\n")
     -- stylua: ignore
     dashboard.section.buttons.val = {
       dashboard.button("f", " " .. " Find file",       "<cmd> lua LazyVim.pick()() <cr>"),
@@ -218,74 +219,145 @@ end
       dashboard.button("l", "󰒲 " .. " Lazy",            "<cmd> Lazy <cr>"),
       dashboard.button("q", " " .. " Quit",            "<cmd> qa <cr>"),
     }
-    for _, button in ipairs(dashboard.section.buttons.val) do
-      button.opts.hl = "AlphaButtons"
-      button.opts.hl_shortcut = "AlphaShortcut"
-    end
-    dashboard.section.header.opts.hl = "AlphaHeader"
-    dashboard.section.buttons.opts.hl = "AlphaButtons"
-    dashboard.section.footer.opts.hl = "AlphaFooter"
-    dashboard.opts.layout[1].val = 8
-    return dashboard
-  end,
-  config = function(_, dashboard)
-    -- close Lazy and re-open when the dashboard is ready
-    if vim.o.filetype == "lazy" then
-      vim.cmd.close()
+      for _, button in ipairs(dashboard.section.buttons.val) do
+        button.opts.hl = "AlphaButtons"
+        button.opts.hl_shortcut = "AlphaShortcut"
+      end
+      dashboard.section.header.opts.hl = "AlphaHeader"
+      dashboard.section.buttons.opts.hl = "AlphaButtons"
+      dashboard.section.footer.opts.hl = "AlphaFooter"
+      dashboard.opts.layout[1].val = 8
+      return dashboard
+    end,
+    config = function(_, dashboard)
+      -- close Lazy and re-open when the dashboard is ready
+      if vim.o.filetype == "lazy" then
+        vim.cmd.close()
+        vim.api.nvim_create_autocmd("User", {
+          once = true,
+          pattern = "AlphaReady",
+          callback = function()
+            require("lazy").show()
+          end,
+        })
+      end
+
+      require("alpha").setup(dashboard.opts)
+
       vim.api.nvim_create_autocmd("User", {
         once = true,
-        pattern = "AlphaReady",
+        pattern = "LazyVimStarted",
         callback = function()
-          require("lazy").show()
+          local stats = require("lazy").stats()
+          local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
+          dashboard.section.footer.val = "⚡ Neovim loaded "
+            .. stats.loaded
+            .. "/"
+            .. stats.count
+            .. " plugins in "
+            .. ms
+            .. "ms"
+          pcall(vim.cmd.AlphaRedraw)
         end,
       })
-    end
-
-    require("alpha").setup(dashboard.opts)
-
-    vim.api.nvim_create_autocmd("User", {
-      once = true,
-      pattern = "LazyVimStarted",
-      callback = function()
-        local stats = require("lazy").stats()
-        local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
-        dashboard.section.footer.val = "⚡ Neovim loaded "
-          .. stats.loaded
-          .. "/"
-          .. stats.count
-          .. " plugins in "
-          .. ms
-          .. "ms"
-        pcall(vim.cmd.AlphaRedraw)
-      end,
-    })
-  end,
-},
-{
-  "akinsho/horizon.nvim",
-  opts = {
-    overrides = {
-      colors = {
-        CursorLine = { bg = '#1e1e1e', fg = '#ffffff', underline = true }
-      }
-    },
-    plugins = {
-      cmp = true,
-      indent_blankline = true,
-      nvim_tree = true,
-      telescope = true,
-      which_key = true,
-      barbar = true,
-      notify = true,
-      symbols_outline = true,
-      neo_tree = true,
-      gitsigns = true,
-      crates = true,
-      hop = true,
-      navic = true,
-      quickscope = true,
-      flash = true,
-    }
+    end,
   },
+{
+  "folke/tokyonight.nvim",
+  lazy = false,
+  priority = 1000,
+  opts = {
+    -- optional: pick a base style you like
+    style = "night",
+    transparent = false,
+
+    -- 1) optionally remap Tokyonight base palette to Horizon palette
+    --    (only if you ALSO want TN's core colors changed)
+    -- on_colors = function(c)
+    --   local H = require("config.highlights_palette")  -- if you have a palette module
+    --   -- Example mappings (tweak as you like):
+    --   c.bg = "#1C1E26"
+    --   c.bg_dark = "#1C1E26"
+    --   c.bg_float = "#232530"
+    --   c.fg = "#BBBBBB"
+    --   c.border = "#232530"
+    --   c.comment = "#4C4D53"
+    --   c.magenta = "#EE64AC"
+    --   c.red = "#E95678"
+    --   c.orange = "#FAB795"
+    --   c.yellow = "#FAB795"
+    --   c.green = "#29D398"
+    --   c.blue = "#26BBD9"
+    --   c.cyan = "#59E1E3"
+    -- end,
+
+    -- 2) apply your Horizon highlight groups
+    on_highlights = function(hl, c)
+      -- load your horizon highlight table
+      -- works whether your module returns { AlphaHeader = {...} }
+      -- or { overrides = { AlphaHeader = {...} } }
+      local mod = require("config.highlights")
+      local horizon = mod.overrides or mod
+
+      -- normalize and apply:
+      for group, spec in pairs(horizon) do
+        if type(spec) == "string" then
+          -- treat "Group" string as a link
+          hl[group] = { link = spec }
+        else
+          -- copy the table as-is (fg/bg/bold/italic/undercurl/etc.)
+          hl[group] = vim.tbl_extend("force", {}, spec)
+        end
+      end
+    end,
+  },
+  config = function(_, opts)
+    require("tokyonight").setup(opts)
+    vim.cmd.colorscheme("tokyonight")
+  end,
 }
+
+  -- {
+  --   "lunarvim/horizon.nvim",
+  --   priority = 1000,
+  --   opts = {
+  --     overrides = {
+  --       colors = {
+  --         AlphaButtons = {
+  --   fg = "#007197", bg = "#1a1c23"
+  -- },
+  -- AlphaFooter = {
+  --   fg = "#188092", bg = "#1a1c23"
+  -- },
+  -- AlphaHeader = {
+  --   fg = "#2e7de9", bg = "#1a1c23"
+  -- },
+  -- AlphaHeaderLabel = {
+  --   fg = "#b15c00", bg = "#1a1c23"
+  -- },
+  -- AlphaShortcut = {
+  --   fg = "#b15c00", bg = "#1a1c23"
+  -- },
+  --       }
+  --     },
+  --     plugins = {
+  --       cmp = true,
+  --       indent_blankline = true,
+  --       nvim_tree = true,
+  --       telescope = true,
+  --       which_key = true,
+  --       barbar = true,
+  --       notify = true,
+  --       symbols_outline = true,
+  --       neo_tree = true,
+  --       gitsigns = true,
+  --       crates = true,
+  --       hop = true,
+  --       navic = true,
+  --       quickscope = true,
+  --       flash = true,
+  --     },
+  --   },
+  -- }
+
 }
