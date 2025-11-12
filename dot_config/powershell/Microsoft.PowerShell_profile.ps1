247340b57c6edf34506efd2edd9e8b8443c38a35
Luis Martins <lmartins@firstpointpower.com>
2025-11-12 10:33:15 -0500

    fix powershell profile to work with chezmoi template

--- dot_config/powershell/Microsoft.PowerShell_profile.ps1
+++
@@ -1,7 +0,0 @@
-Invoke-Expression (&/opt/homebrew/bin/starship  init powershell)
-Import-Module -Name Terminal-Icons
-$(/opt/homebrew/bin/brew shellenv) | Invoke-Expression
-$env:PATH = "$HOME/.local/bin:" + $env:PATH
-$env:GOPATH = "$HOME/go"
-Set-Alias cura cursor-agent
-Set-Alias libgen ~/go/bin/libgen-cli
