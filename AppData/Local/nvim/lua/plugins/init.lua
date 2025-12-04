ae96d54b6eaf8378c3b3adfeb712b1f997fb847f
Luis Martins <lmartins@firstpointpower.com>
2025-12-04 08:31:14 -0500

    add lazyvim to chezmoi

---
+++ AppData/Local/nvim/lua/plugins/init.lua
@@ -0,0 +1,329 @@
+return {
+  -- {
+  --   "stevearc/conform.nvim",
+  --   -- event = 'BufWritePre', -- uncomment for format on save
+  --   opts = require "configs.conform",
+  -- },
+
+  { "neovim/nvim-lspconfig" },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "hyprlang",
+        "vim",
+        "lua",
+        "vimdoc",
+        "html",
+        "css",
+        "c_sharp",
+        "razor",
+      },
+    },
+  },
+  {
+    "mason-org/mason.nvim",
+    opts = {
+      registries = {
+        "github:mason-org/mason-registry",
+        "github:Crashdummyy/mason-registry",
+      },
+      ensure_installed = {
+        "lua-language-server",
+        "xmlformatter",
+        "csharpier",
+        "prettier",
+        "stylua",
+        "bicep-lsp",
+        "html-lsp",
+        "css-lsp",
+        "eslint-lsp",
+        "typescript-language-server",
+        "json-lsp",
+        "rust-analyzer",
+        "roslyn",
+        "rzls",
+      },
+    },
+  },
+  {
+    "seblyng/roslyn.nvim",
+    ---@module 'roslyn.config'
+    ---@type RoslynNvimConfig
+    ft = { "cs", "razor" },
+    opts = {},
+
+    dependencies = {
+      {
+        "tris203/rzls.nvim",
+        config = true,
+      },
+    },
+    lazy = false,
+    config = function()
+      -- Use one of the methods in the Integration section to compose the command.
+      local mason_registry = require("mason-registry")
+
+      local rzls_path = vim.fn.expand("$MASON/packages/rzls/libexec")
+      local cmd = {
+        "roslyn",
+        "--stdio",
+        "--logLevel=Information",
+        "--extensionLogDirectory=" .. vim.fs.dirname(vim.lsp.get_log_path()),
+        "--razorSourceGenerator=" .. vim.fs.joinpath(rzls_path, "Microsoft.CodeAnalysis.Razor.Compiler.dll"),
+        "--razorDesignTimePath=" .. vim.fs.joinpath(rzls_path, "Targets", "Microsoft.NET.Sdk.Razor.DesignTime.targets"),
+        "--extension",
+        vim.fs.joinpath(rzls_path, "RazorExtension", "Microsoft.VisualStudioCode.RazorExtension.dll"),
+      }
+
+      vim.lsp.config("roslyn", {
+        cmd = cmd,
+        handlers = require("rzls.roslyn_handlers"),
+        settings = {
+          ["csharp|inlay_hints"] = {
+            csharp_enable_inlay_hints_for_implicit_object_creation = true,
+            csharp_enable_inlay_hints_for_implicit_variable_types = true,
+
+            csharp_enable_inlay_hints_for_lambda_parameter_types = true,
+            csharp_enable_inlay_hints_for_types = true,
+            dotnet_enable_inlay_hints_for_indexer_parameters = true,
+            dotnet_enable_inlay_hints_for_literal_parameters = true,
+            dotnet_enable_inlay_hints_for_object_creation_parameters = true,
+            dotnet_enable_inlay_hints_for_other_parameters = true,
+            dotnet_enable_inlay_hints_for_parameters = true,
+            dotnet_suppress_inlay_hints_for_parameters_that_differ_only_by_suffix = true,
+            dotnet_suppress_inlay_hints_for_parameters_that_match_argument_name = true,
+            dotnet_suppress_inlay_hints_for_parameters_that_match_method_intent = true,
+          },
+          ["csharp|code_lens"] = {
+            dotnet_enable_references_code_lens = true,
+          },
+        },
+      })
+      vim.lsp.enable("roslyn")
+    end,
+    init = function()
+      -- We add the Razor file types before the plugin loads.
+      vim.filetype.add({
+        extension = {
+          razor = "razor",
+          cshtml = "razor",
+        },
+      })
+    end,
+  },
+  { "nvim-telescope/telescope-ui-select.nvim" },
+  {
+    "folke/persistence.nvim",
+    event = "BufReadPre",
+    opts = {},
+  -- stylua: ignore
+  keys = {
+    { "<leader>qs", function() require("persistence").load() end, desc = "Restore Session" },
+    { "<leader>qS", function() require("persistence").select() end,desc = "Select Session" },
+    { "<leader>ql", function() require("persistence").load({ last = true }) end, desc = "Restore Last Session" },
+    { "<leader>qd", function() require("persistence").stop() end, desc = "Don't Save Current Session" },
+  },
+  },
+  {
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    opts = {
+      lsp = {
+        override = {
+          ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+          ["vim.lsp.util.stylize_markdown"] = true,
+          ["cmp.entry.get_documentation"] = true,
+        },
+      },
+      routes = {
+        {
+          filter = {
+            event = "msg_show",
+            any = {
+              { find = "%d+L, %d+B" },
+              { find = "; after #%d+" },
+              { find = "; before #%d+" },
+            },
+          },
+          view = "mini",
+        },
+      },
+      presets = {
+        bottom_search = true,
+        command_palette = true,
+        long_message_to_split = true,
+      },
+    },
+  -- stylua: ignore
+  keys = {
+    { "<leader>sn", "", desc = "+noice"},
+    { "<S-Enter>", function() require("noice").redirect(vim.fn.getcmdline()) end, mode = "c", desc = "Redirect Cmdline" },
+    { "<leader>snl", function() require("noice").cmd("last") end, desc = "Noice Last Message" },
+    { "<leader>snh", function() require("noice").cmd("history") end, desc = "Noice History" },
+    { "<leader>sna", function() require("noice").cmd("all") end, desc = "Noice All" },
+    { "<leader>snd", function() require("noice").cmd("dismiss") end, desc = "Dismiss All" },
+    { "<leader>snt", function() require("noice").cmd("pick") end, desc = "Noice Picker (Telescope/FzfLua)" },
+    { "<c-f>", function() if not require("noice.lsp").scroll(4) then return "<c-f>" end end, silent = true, expr = true, desc = "Scroll Forward", mode = {"i", "n", "s"} },
+    { "<c-b>", function() if not require("noice.lsp").scroll(-4) then return "<c-b>" end end, silent = true, expr = true, desc = "Scroll Backward", mode = {"i", "n", "s"}},
+  },
+    config = function(_, opts)
+      -- HACK: noice shows messages from before it was enabled,
+      -- but this is not ideal when Lazy is installing plugins,
+      -- so clear the messages in this case.
+      if vim.o.filetype == "lazy" then
+        vim.cmd([[messages clear]])
+      end
+      require("noice").setup(opts)
+      local orig = vim.lsp.handlers["$/progress"]
+      vim.lsp.handlers["$/progress"] = function(err, result, ctx, config)
+        if not (result and result.token) then
+          return
+        end
+        return orig(err, result, ctx, config)
+      end
+    end,
+  },
+  {
+    "goolord/alpha-nvim",
+    event = "VimEnter",
+    enabled = true,
+    init = false,
+    opts = function()
+      local dashboard = require("alpha.themes.dashboard")
+      local logo = [[
+
+  ⠀⠲⣦⣤⣀⣀⠀⠀⠀⣀⣀⣠⣤⣀⣀⠀⢀⣀⣠⣤⣶⣶⠟⠀⠀⠀
+⠀⠀⠀⠀⠙⣿⣿⣿⣿⣷⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠋⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠈⢿⣿⣿⠻⣿⣿⣿⣿⣿⣿⣿⠟⢻⣿⣿⡿⠃⠀⠀⠀⠀⠀
+⠀⠀⠀⠲⣶⣶⣾⣿⣿⠀⢨⠙⢿⣿⣿⠏⣅⠀⢸⣿⣿⣷⣾⠟⠁⠀⠀⠀
+⠀⠀⠀⠀⠈⠻⢿⣿⣿⢷⣶⣶⣾⣿⣿⣶⣶⣾⠟⣿⣿⣿⣋⠀⠀⠀⠀⠀
+⠀⠀⢀⣀⣀⠐⢶⣿⣿⣧⠁⠀⠋⠁⠈⠋⠀⢀⣾⣿⣿⡿⣷⣶⠀⠀⠀⠀
+⠀⠀⣼⣿⣿⣷⣤⣙⣿⣿⣷⣶⣶⣴⣴⣴⣶⣿⣿⣿⠟⣡⣿⣿⣧⣄⣀⡀
+⢀⣤⣿⣿⣿⣿⣿⣿⣿⣿⢿⣿⣿⣿⣿⣿⣿⣿⣿⠿⢿⣿⡿⠿⣿⣿⣿⣿
+⣿⡿⠛⠿⠟⠉⠉⠉⠸⠋⠀⠻⡿⣿⣿⣿⣿⠻⠇⠀⠀⠈⠀⠀⠈⠉⢸⠃
+⠛⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠈⢿⢻⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠹⠀⠀⠀⠀⠀⠀⠀
+    ]]
+
+      dashboard.section.header.val = vim.split(logo, "\n")
+    -- stylua: ignore
+    dashboard.section.buttons.val = {
+      dashboard.button("f", " " .. " Find file",       "<cmd> lua LazyVim.pick()() <cr>"),
+      dashboard.button("n", " " .. " New file",        [[<cmd> ene <BAR> startinsert <cr>]]),
+      dashboard.button("r", " " .. " Recent files",    [[<cmd> lua LazyVim.pick("oldfiles")() <cr>]]),
+      dashboard.button("g", " " .. " Find text",       [[<cmd> lua LazyVim.pick("live_grep")() <cr>]]),
+      dashboard.button("c", " " .. " Config",          "<cmd> lua LazyVim.pick.config_files()() <cr>"),
+      dashboard.button("s", " " .. " Restore Session", [[<cmd> lua require("persistence").load() <cr>]]),
+      dashboard.button("x", " " .. " Lazy Extras",     "<cmd> LazyExtras <cr>"),
+      dashboard.button("l", "󰒲 " .. " Lazy",            "<cmd> Lazy <cr>"),
+      dashboard.button("q", " " .. " Quit",            "<cmd> qa <cr>"),
+    }
+      for _, button in ipairs(dashboard.section.buttons.val) do
+        button.opts.hl = "AlphaButtons"
+        button.opts.hl_shortcut = "AlphaShortcut"
+      end
+      dashboard.section.header.opts.hl = "AlphaHeader"
+      dashboard.section.buttons.opts.hl = "AlphaButtons"
+      dashboard.section.footer.opts.hl = "AlphaFooter"
+      dashboard.opts.layout[1].val = 8
+      return dashboard
+    end,
+    config = function(_, dashboard)
+      -- close Lazy and re-open when the dashboard is ready
+      if vim.o.filetype == "lazy" then
+        vim.cmd.close()
+        vim.api.nvim_create_autocmd("User", {
+          once = true,
+          pattern = "AlphaReady",
+          callback = function()
+            require("lazy").show()
+          end,
+        })
+      end
+
+      require("alpha").setup(dashboard.opts)
+
+      vim.api.nvim_create_autocmd("User", {
+        once = true,
+        pattern = "LazyVimStarted",
+        callback = function()
+          local stats = require("lazy").stats()
+          local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
+          dashboard.section.footer.val = "⚡ Neovim loaded "
+            .. stats.loaded
+            .. "/"
+            .. stats.count
+            .. " plugins in "
+            .. ms
+            .. "ms"
+          pcall(vim.cmd.AlphaRedraw)
+        end,
+      })
+    end,
+  },
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {
+      -- optional: pick a base style you like
+      style = "night",
+      transparent = false,
+
+      -- 1) optionally remap Tokyonight base palette to Horizon palette
+      --    (only if you ALSO want TN's core colors changed)
+      -- on_colors = function(c)
+      --   local H = require("config.highlights_palette")  -- if you have a palette module
+      --   -- Example mappings (tweak as you like):
+      --   c.bg = "#1C1E26"
+      --   c.bg_dark = "#1C1E26"
+      --   c.bg_float = "#232530"
+      --   c.fg = "#BBBBBB"
+      --   c.border = "#232530"
+      --   c.comment = "#4C4D53"
+      --   c.magenta = "#EE64AC"
+      --   c.red = "#E95678"
+      --   c.orange = "#FAB795"
+      --   c.yellow = "#FAB795"
+      --   c.green = "#29D398"
+      --   c.blue = "#26BBD9"
+      --   c.cyan = "#59E1E3"
+      -- end,
+
+      -- 2) apply your Horizon highlight groups
+      on_highlights = function(hl, c)
+        -- load your horizon highlight table
+        -- works whether your module returns { AlphaHeader = {...} }
+        -- or { overrides = { AlphaHeader = {...} } }
+        local mod = require("config.highlights")
+        local horizon = mod.overrides or mod
+
+        -- normalize and apply:
+        for group, spec in pairs(horizon) do
+          if type(spec) == "string" then
+            -- treat "Group" string as a link
+            hl[group] = { link = spec }
+          else
+            -- copy the table as-is (fg/bg/bold/italic/undercurl/etc.)
+            hl[group] = vim.tbl_extend("force", {}, spec)
+          end
+        end
+      end,
+    },
+    config = function(_, opts)
+      require("tokyonight").setup(opts)
+      vim.cmd.colorscheme("tokyonight")
+    end,
+  },
+  {
+    "Kurren123/mssql.nvim",
+    opts = {
+      -- optional
+      keymap_prefix = "<leader>m",
+    },
+    -- optional
+    dependencies = { "folke/which-key.nvim" },
+  },
+}
+
